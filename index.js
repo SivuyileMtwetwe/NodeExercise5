2c26493c5055dbcecbfcc8bddcf319f241e0f789
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2025-01-30 13:39:22 +0200

    ex done

--- index.js
+++ index.js
@@ -5,19 +5,79 @@ config();
 
 const PORT = process.env.PORT
 
-const app = express()
-
-app.listen(PORT,()=>{
-    console.log('http://127.0.0.1:'+ PORT);
-    })
-    
-
 const pool = mysql.createPool({
     host: process.env.HOST,
     user:process.env.USER,
     password:process.env.PASSWORD,
     database:process.env.DATABASE}
 )
+const app = express()
+app.use(express.json())
+
+
+app.get('/products', async (req,res)=>{
+    res.json({products: await getAllProducts()})
+})
+
+app.get('/users', async (req,res)=>{
+    res.json({users: await getAllUsers()})
+})
+
+app.post('/products', async(req,res)=>{
+    console.log( req.body);
+
+
+    let {product_code,product_name,product_price,product_quantity} = req.body
+    
+    res.json({product: await addProduct(product_code,product_name,product_price,product_quantity)})
+})
+
+app.post('/users', async(req,res)=>{
+    console.log( req.body);
+    
+    
+    let {email,first_name,last_name,password} = req.body
+    
+    res.json({product: await addUser(email,first_name,last_name,password)})
+})
+
+app.delete("/products/:product_code", async (req, res) => {
+    
+    res.json({ products: await deleteProduct(req.params.product_code) })
+    
+});
+
+app.delete("/users/:id", async (req, res) => {
+    
+    res.json({ user: await deleteUser(req.params.id) })
+    
+});
+
+app.delete("/products", async (req,res)=>{
+    
+    res.json({products: await deleteAllProducts()})
+})
+
+app.delete("/users", async (req,res)=>{
+    
+    res.json({products: await deleteAllUsers()})
+})
+
+
+app.patch('/products/:product_code', async (req, res) => {
+        const { product_name, product_price, product_quantity } = req.body;
+        const { product_code } = req.params;
+
+        res.json({ products:await updateProduct(product_name,product_price,product_quantity,product_code) });
+});
+
+app.patch('/users/:id', async (req, res) => {
+        const { email,first_name,last_name,password} = req.body;
+        const { id } = req.params;
+
+        res.json({ products:await updateUser(email,first_name,last_name,password,id) });
+});
+
 
 
 const getAllProducts = async ()=>{
@@ -48,30 +108,63 @@ const getUser = async (id)=>{
 
 console.log(await getUser());
 
-// const addProduct = async (product_code, first_name,last_name,email,phone_number,department,salary)=>{
-//     await pool.query("INSERT INTO employees (product_code, first_name,last_name,email,phone_number,department,salary) VALUES (?,?,?,?,?,?)",[first_name,last_name,email,phone_number,department,salary])
-//     return await getAllProducts()
-// }
+const addProduct = async (product_code,product_name,product_price,product_quantity)=>{
+    await pool.query("INSERT INTO products (product_code,product_name,product_price,product_quantity) VALUES (?,?,?,?)",[product_code,product_name,product_price,product_quantity])
+    return await getAllProducts()
+}
+
+// console.log(await addProduct("amas1", "Amasi Full Cream", 35.99, 19));
+
+const addUser = async (email,first_name,last_name,password)=>{
+    await pool.query('INSERT INTO users (email,first_name,last_name,password) VALUES (?,?,?, ?);',[email,first_name,last_name,password])
+    return await getAllUsers()
+}
+
+// console.log(await addUser("khanyiso@lifechoices.co.za","khanyiso","haman", "khanyisohaman"));
+
+const deleteAllProducts = async ()=>{
+    await pool.query("TRUNCATE TABLE products")
+}
+// console.log(await deleteAllProducts());
+
+const deleteAllUsers = async ()=>{
+    await pool.query("TRUNCATE TABLE users")
+    return await getAllUsers()
+}
+// console.log(await deleteAllUsers());
+
+const deleteProduct = async (product_code)=>{
+    await pool.query("DELETE FROM products WHERE product_code LIKE  ?",[product_code])
+    return await getAllProducts()
+}
+// console.log(await deleteProduct("hscc1"));
+
+const deleteUser = async (id)=>{
+    await pool.query("DELETE FROM users WHERE id LIKE  ?",[id])
+    return await getAllUsers()
+}
+
+// console.log(await deleteUser(1));
+
+const updateProduct = async (product_name,product_price,product_quantity,product_code)=>{
+    await pool.query("UPDATE products SET product_name=?,product_price =?,product_quantity =? WHERE product_code = ?",[product_name,product_price,product_quantity,product_code])
+    return await getAllProducts()
+}
+
+// console.log(await updateProduct("Sunlight Liquid", 25.99, 80,"slhs1"));
 
 
-// console.log(await addEmployee("Sivuyile","Mtwetwe", "sivuyilemtwetwe@gmail.com", "082-6254-659","Cybersecurity",100000.00));
+
+const updateUser = async (email,first_name,last_name,password,id)=>{
+    await pool.query("UPDATE users SET email=?,first_name =?,last_name =?, password=? WHERE id = ?",[email,first_name,last_name,password,id])
+    return await getAllUsers()
+}
+
+// console.log(await updateUser("mtwetwe@gmail.com", "Sivuyile", "Mtwetwe","MaybeShouldBeclearText123!",2));
 
 
 
-// const deleteEmployee = async (product_code)=>{
-//     await pool.query("DELETE FROM pick_n_steal.employees WHERE product_code LIKE  ?",[product_code])
-//     return await getAllEmployees()
-// }
-
-
-// console.log(await deleteEmployee(4));
-
-// const updateEmployee = async (first_name,last_name,email,phone_number,department,salary,product_code)=>{
-//     await pool.query("UPDATE employees SET first_name=?,last_name =?,email =?,phone_number =?,department =?,salary =? WHERE product_code = ?",[first_name,last_name,email,phone_number,department,salary,product_code])
-//     return await getAllEmployees()
-// }
-
-
-// console.log(await updateEmployee("Jane","Doe","jane.doe@updated.com","444-666", "IT",90000.00,1));
-
-
+app.listen(PORT,()=>{
+    console.log('http://127.0.0.1:'+ PORT);
+    })
+    
